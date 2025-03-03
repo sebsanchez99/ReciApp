afb54c1d13abbbf9185e000ab30464db51f8cb83
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-03 15:31:36 -0500

    se crea dml inicial con datos predeterminados

---
+++ database/DML.sql
@@ -0,0 +1,9 @@
+-- Insertar los roles iniciales
+INSERT INTO rol (nombre) VALUES ('administrador'), ('reciclador'), ('empresa');
+
+-- Inserta tipos de documento
+INSERT INTO tipo_documento (nombre) VALUES
+('Cédula de Ciudadanía'),
+('Tarjeta de Identidad'),
+('NIT'),
+('Pasaporte');
